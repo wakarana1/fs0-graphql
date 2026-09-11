b5bcca5cf0494e2fb7f3971a042369b582a71e8c
wakarana1 <wu.christophert@gmail.com>
2026-09-10 18:12:34 -0700

    graphql init and library-backend chapter 2 done

---
+++ library-backend/index.js
@@ -0,0 +1,206 @@
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
+
+/*
+ * Suomi:
+ * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
+ * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
+ *
+ * English:
+ * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
+ * However, for simplicity, we will store the author's name in connection with the book
+ *
+ * Spanish:
+ * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
+ * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
+ */
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "Demons",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+]
+
+/*
+  you can remove the placeholder query once your first one has been implemented 
+*/
+
+const typeDefs = /* GraphQL */ `
+  type Book {
+    id: ID!
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    born: Int
+    bookCount: Int
+  }
+  type Query {
+    dummy: Int
+    bookCount: Int
+    authorCount: Int
+    allAuthors: [Author!]!
+    allBooks(author: String, genre: String): [Book!]!
+  }
+  type Mutation {
+    addBook(
+      title: String!,
+      published: Int!,
+      author: String!,
+      genres: [String!]!
+    ): Book!
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`
+
+const resolvers = {
+  Book: {
+    id: (book) => book.id,
+    title: (book) => book.title,
+    published: (book) => book.published,
+    author: (book) => book.author,
+    genres: (book) => book.genres,
+  },
+  Author: {
+    id: (author) => author.id,
+    name: (author) => author.name,
+    born: (author) => author.born,
+  },
+  Query: {
+    dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allAuthors: () => authors.map((author) => ({
+      id: author.id,
+      name: author.name,
+      born: author.born,
+      bookCount: books.filter((book) => book.author === author.name).length,
+    })),
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      }
+      if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre))
+      }
+      return books
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.some((author) => author.name === args.author)) {
+        const newAuthor = {
+          name: args.author,
+          id: crypto.randomUUID(),
+        }
+        authors.push(newAuthor)
+      }
+      const author = authors.find((author) => author.name === args.author)
+
+      const newBook = {
+        title: args.title,
+        author: args.author,
+        published: args.published,
+        genres: args.genres,
+        id: crypto.randomUUID(),
+      }
+      books.push(newBook)
+      return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name)
+      if (!author) {
+        return null
+      }
+      author.born = args.setBornTo
+      return author
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
